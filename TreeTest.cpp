f46aa4e7e484835ba24dfb58f0ff46898b266d04
Yotam124 <44746421+Yotam124@users.noreply.github.com>
2019-03-24 17:18:49 +0200

    Update TreeTest.cpp

--- TreeTest.cpp
+++ TreeTest.cpp
@@ -22,6 +22,14 @@ int main() {
   threetree.insert(3);
 
   ariel::Tree mytree;
+  mytree.insert(50);
+  mytree.insert(25);
+  mytree.insert(75);
+  mytree.insert(30);
+  mytree.insert(60);
+  mytree.insert(55);
+  mytree.insert(52);
+
 
   badkan::TestCase tc("Binary tree");
   tc
@@ -43,7 +51,25 @@ int main() {
   .CHECK_THROWS(threetree.insert(3))
   .CHECK_THROWS(threetree.left(6))
   .CHECK_OK    (threetree.print())
+
+  .CHECK_EQUAL (mytree.right(mytree.Root->value), 75)
+  .CHECK_EQUAL (mytree.size(), 7)
+  .CHECK_OK (mytree.remove(75))
+  .CHECK_EQUAL (mytree.size(), 6)
+  .CHECK_EQUAL (mytree.right(mytree.Root->value), 60)
+  .CHECK_THROWS (mytree.parent(mytree.Root->value))
+  .CHECK_EQUAL (mytree.parent(55), 60)
+  .CHECK_OK    (emptytree.insert(10))
+  .CHECK_OK    (emptytree.insert(90))
+  .CHECK_EQUAL (mytree.parent(25), mytree.Root->value)
+  .CHECK_EQUAL (mytree.left(55), 52)
+  .CHECK_EQUAL (mytree.right(25), 30)
+  .CHECK_OK (mytree.print())
   .print();
+
+
+
+
 
   cout << "You have " << tc.right() << " right answers and " << tc.wrong() << " wrong answers so your grade is " << tc.grade() << ". Great!" << endl;
 }
